1f6b1bce8324c2d13b24b3faa4647095506ffd68
Ricky Gonzalez <rickygonzalez@gmail.com>
2020-02-11 03:01:46 -0600

    installed packages

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require('express')
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const publicPath = path.join(__dirname, "/../public");
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+io.on('connection', function(socket){
+  console.log('a user connected. User:', socket.id);
+  socket.on('disconnect', ()=>console.log("A user has disconnected. User: ", socket.id))
+});
+
+http.listen(3000, function(){
+  console.log(`listening on *:${port}`);
+});
